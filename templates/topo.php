664fb9415a6a1f7e962445f76c65aaa9c07c9ca4
Ludimila-Araujo <dearaujo.ludimila@gmail.com>
2024-09-30 21:06:34 -0300

    Add files via upload

---
+++ templates/topo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Padaria_Plus</title>
+    <link rel="stylesheet" href="estilo.padaria.plus.css">
+</head>
+<body>
+<body>
+    <div class="secao">
+        <img class="imagem-logotipo" src="logo.png" alt="Logo da Padaria Plus">
+    </div>
+    
+</body>
+</html>
